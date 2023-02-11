ddf35f1c80f64d5fb9bfc97d91af599be401e746
Fjallroth <christer.fjallroth@gmail.com>
2023-02-11 19:22:51 +0100

    tailwind styling

--- controllers/todos.js
+++ controllers/todos.js
@@ -204,7 +204,7 @@ module.exports = {
     getTodos: async (req,res)=>{
         console.log(req.user)
         try{
-            const todoItems = await Todo.find({userId:req.user.id}).sort({rank: 1})
+            const todoItems = await Todo.find({userId:req.user.id}).sort({percentageOff: 1})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
             res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})          
         }catch(err){
